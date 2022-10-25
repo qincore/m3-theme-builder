eec057d01986f1c79bf8b96d22082d47f1e283b2
zeir <andyhuyuxin@outlook.com>
2022-10-25 23:56:58 +0800

    perf: 优化颜色混合方法

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,29 +1,15 @@
 import { useLocation } from 'react-router-dom'
-import { normal } from 'color-blend'
 import styles from './style.module.less'
 import Navbar from '@/components/Navbar'
 import Router from '@/routes'
 import routes from '@/routes/routes'
-import { argbAddAlpha, blendColors, hexFromArgb } from '@/utils/color_utils'
 
 const Layout = () => {
   const { pathname } = useLocation()
-  const fa = argbAddAlpha(4278213063, 0.08)
-  const hc = blendColors(4294900735, fa)
-  const hex = hexFromArgb(hc.argb)
-  const rgba = normal({ r: 254, g: 251, b: 255, a: 1 }, { r: 0, g: 89, b: 199, a: 0.08 })
   return (
     <>
       <Navbar pathname={pathname} menu={routes} />
       <main className={styles['app-main']}>
-        <div style={{ width: '200px', height: '200px', backgroundColor: hex }} />
-        <div
-          style={{
-            width: '200px',
-            height: '200px',
-            backgroundColor: `rgba(${rgba.r}, ${rgba.g}, ${rgba.b}, ${rgba.a})`
-          }}
-        />
         <Router />
       </main>
     </>
--- src/utils/color_utils.ts
+++ src/utils/color_utils.ts
@@ -1,46 +1,46 @@
 import { alphaFromArgb, blueFromArgb, greenFromArgb, redFromArgb } from '@material/material-color-utilities'
 
 /**
+ * @description 将背景色和前景色混合成新的颜色，主要用于计算出surface高程颜色.
  * @param back ARGB 数字.
  * @param fore ARGB 数字.
- * @return 混合后的的 ARGB 数字.
+ * @param colorMode 输出的颜色模式 argb ｜ rgba ｜ hex.
+ * @return 混合后 ARGB 数字 ｜ rgba rgba(x,x,x,x) ｜ hex #ff0844.
  */
-export const blendColors = (back: number, fore: number): { argb: number; rgba: string } => {
-  const backArr = [
-    redFromArgb(back),
-    greenFromArgb(back),
-    blueFromArgb(back),
-    Number((alphaFromArgb(back) / 255).toFixed(2))
-  ]
-  const foreArr = [
-    redFromArgb(fore),
-    greenFromArgb(fore),
-    blueFromArgb(fore),
-    Number((alphaFromArgb(fore) / 255).toFixed(2))
-  ]
-  const res = []
-  // alpha
-  res[3] = 1 - (1 - foreArr[3]) * (1 - backArr[3])
-
-  // red
-  res[0] = Math.round((foreArr[0] * foreArr[3]) / res[3] + (backArr[0] * backArr[3] * (1 - foreArr[3])) / res[3])
-
-  // green
-  res[1] = Math.round((foreArr[1] * foreArr[3]) / res[3] + (backArr[1] * backArr[3] * (1 - foreArr[3])) / res[3])
-
-  // blue
-  res[2] = Math.round((foreArr[2] * foreArr[3]) / res[3] + (backArr[2] * backArr[3] * (1 - foreArr[3])) / res[3])
-
-  console.log(backArr, foreArr, res)
-
-  return {
-    // eslint-disable-next-line no-bitwise
-    argb: (((res[3] * 255) << 24) | ((res[0] & 255) << 16) | ((res[1] & 255) << 8) | (res[2] & 255)) >>> 0,
-    rgba: `rgba(${res[0]}, ${res[1]}, ${res[2]}, ${res[3]})`
+export const blendColors = (back: number, fore: number, colorMode: string): string | number => {
+  const backRGBA = {
+    r: redFromArgb(back),
+    g: greenFromArgb(back),
+    b: blueFromArgb(back),
+    a: alphaFromArgb(back) / 255
   }
+  const foreRGBA = {
+    r: redFromArgb(fore),
+    g: greenFromArgb(fore),
+    b: blueFromArgb(fore),
+    a: alphaFromArgb(fore) / 255
+  }
+  const a = 1 - (1 - foreRGBA.a) * (1 - backRGBA.a)
+  const r = Math.round((foreRGBA.r * foreRGBA.a) / a + (backRGBA.r * backRGBA.a * (1 - foreRGBA.a)) / a)
+  const g = Math.round((foreRGBA.g * foreRGBA.a) / a + (backRGBA.g * backRGBA.a * (1 - foreRGBA.a)) / a)
+  const b = Math.round((foreRGBA.b * foreRGBA.a) / a + (backRGBA.b * backRGBA.a * (1 - foreRGBA.a)) / a)
+
+  if (colorMode === 'hex') {
+    const rgbaArr = [r.toString(16), g.toString(16), b.toString(16), (a * 255).toString(16)]
+    return `#${rgbaArr.map((item) => (item.length === 1 ? `0${item}` : item)).join('')}`
+  }
+
+  if (colorMode === 'rgba') {
+    return `rgba(${r},${g},${b},${a})`
+  }
+
+  // ARGB
+  // eslint-disable-next-line no-bitwise
+  return ((a << 24) | ((r & 255) << 16) | ((g & 255) << 8) | (b & 255)) >>> 0
 }
 
 /**
+ * @description 将 ARGB 转换为 hex WEB颜色字符串.
  * @param argb ARGB 数字.
  * @return Hex 16进制字符串色值, 如：红色 #ff0000.
  */
@@ -62,6 +62,7 @@ export const hexFromArgb = (argb: number): string => {
 }
 
 /**
+ * @description 给 ARGB 添加透明度.
  * @param argb ARGB 数字.
  * @param alpha 需要添加的透明度 0 - 1.
  * @return 加入透明度的 ARGB 数字.
