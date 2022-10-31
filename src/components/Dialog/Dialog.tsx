a053a420bb4b7e1fa529e7a78c3be994240de08d
huyuxin <hyx01220526@antgroup.com>
2022-10-31 18:31:49 +0800

    style: dialog进出动画

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useMemo, useState } from 'react'
+import React, { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
 import classnames from 'classnames'
 import styles from './style.module.less'
 import Button from '@/components/Button'
@@ -23,9 +23,12 @@ const Dialog = (props: IDialogProps) => {
 
   const [visible, setVisible] = useState<boolean>(false)
   const [en, setEn] = useState<boolean>(false)
+  const [dcHeight, setDcHeight] = useState<number>(220)
   const hasIcon = useMemo(() => {
     return !!icon
   }, [icon])
+
+  const dialogContentRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     setVisible(true)
@@ -43,12 +46,18 @@ const Dialog = (props: IDialogProps) => {
     }
   }, [visible])
 
+  useEffect(() => {
+    if (dialogContentRef.current && visible) {
+      setDcHeight(dialogContentRef.current.clientHeight)
+    }
+  }, [dialogContentRef, visible])
+
   const close = () => {
     setEn(false)
     setTimeout(() => {
       setVisible(false)
       onClose?.()
-    }, 300)
+    }, 500)
   }
 
   const ok = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -68,26 +77,33 @@ const Dialog = (props: IDialogProps) => {
     <div className={classnames(styles.dialog, { [styles.en]: en })}>
       <div className={styles.dialogMask} />
       <div className={styles.dialogWrapper}>
-        <div className={classnames(styles.dialogContent, { [styles.hasIcon]: hasIcon })}>
-          <div className={styles.dialogHeader}>
-            <div className={styles.dialogIcon}>
-              {typeof icon === 'string' ? (
-                <span className={classnames('material-icons-outlined', styles.mdIcon)}>{icon}</span>
-              ) : (
-                icon
-              )}
-            </div>
-            <div className={styles.dialogTitle}>{title}</div>
-          </div>
-          <div className={styles.dialogBody}>{content}</div>
-          <div className={styles.dialogFooter}>
-            <div className={styles.buttonBar}>
-              <Button size="small" style={{ marginRight: '8px' }} onClick={(e) => cancel(e)} type="text">
-                {cancelText}
-              </Button>
-              <Button size="small" onClick={(e) => ok(e)} type="text">
-                {okText}
-              </Button>
+        <div
+          style={{ height: `${dcHeight}px` }}
+          className={classnames(styles.dialogContentWrapper, { [styles.hasIcon]: hasIcon })}
+        >
+          <div style={{ height: `${en ? dcHeight : 0}px` }} className={styles.dialogContent}>
+            <div ref={dialogContentRef} className={styles.dialogContentInner}>
+              <div className={styles.dialogHeader}>
+                <div className={styles.dialogIcon}>
+                  {typeof icon === 'string' ? (
+                    <span className={classnames('material-icons-outlined', styles.mdIcon)}>{icon}</span>
+                  ) : (
+                    icon
+                  )}
+                </div>
+                <div className={styles.dialogTitle}>{title}</div>
+              </div>
+              <div className={styles.dialogBody}>{content}</div>
+              <div className={styles.dialogFooter}>
+                <div className={styles.buttonBar}>
+                  <Button size="small" style={{ marginRight: '8px' }} onClick={(e) => cancel(e)} type="text">
+                    {cancelText}
+                  </Button>
+                  <Button size="small" onClick={(e) => ok(e)} type="text">
+                    {okText}
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
